0f9e67266401f7fb74f4196628292cd6693ae47c
Hardik <hardikg672@gmail.com>
2022-11-08 11:55:11 +0530

    Second Commit 2:00:01

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,9 +6,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title:const Text("NMIMS")),
       body: Center(
         child: Text("Hello Hardik $day"),
       ),
+      drawer: Drawer(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mess_menu/home_page.dart';
+import 'package:mess_menu/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(){
   runApp(MyApp());
@@ -12,7 +14,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+        routes:{
+          "/login" :(context) => HomePage(),
+          "/" :(context) => LoginPage(),
+        }
     );
   }
 }
