bc27f09808811432d05a185864904decaddcb623
Hardik <hardikg672@gmail.com>
2022-11-20 09:09:01 +0530

    Ninth Commit 5:02:29

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:mess_menu/models/catalog.dart';
 import 'package:mess_menu/widgets/drawer.dart';
 
-import '../widgets/item_widget.dart';
+// import '../widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -38,14 +38,47 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title:const Text("NMIMS")),
       body:(CatalogModel.items != null && CatalogModel.items.isNotEmpty)?
-      ListView.builder(
-             itemCount: CatalogModel.items.length,
-             itemBuilder: (BuildContext context, int index) {
-               return ItemWidget(
-               item: CatalogModel.items[index],
-               );
-           },
-    ):const Center(
+          GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16
+              ),
+              itemBuilder: (context, index){
+                final item= CatalogModel.items[index];
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  child: GridTile(
+                    header: Container(
+                        child: Text(
+                          item.name,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(color: Colors.deepPurple),),
+                    child: Image.network(item.image),
+                    footer: Container(
+                      child: Text(
+                        item.price.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(color: Colors.deepPurple),),
+                  ),
+                );
+              },
+              itemCount: CatalogModel.items.length,
+          )
+      // ListView.builder(
+      //        itemCount: CatalogModel.items.length,
+      //        itemBuilder: (BuildContext context, int index) {
+      //          return ItemWidget(
+      //          item: CatalogModel.items[index],
+      //          );
+      //      },
+    // )
+        :const Center(
         child: CircularProgressIndicator(),
       ),
       drawer: const MyDrawer(),
